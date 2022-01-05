d0b4927fb30559d1e7d7959b3c2f4407e4d2cd3d
lmbelo <lucas.belo@live.com>
2022-01-05 07:47:02 -0800

    Extending Conda support

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-import setuptools, os, sys, platform, shutil
+import setuptools, os, sys, json, platform, shutil, distutils.dir_util
+from pathlib import Path
+from setuptools.command.install import install
+from setuptools.command.develop import develop
 
 pkgname = "delphivcl"
 
@@ -8,118 +11,94 @@ try:
   class bdist_wheel(_bdist_wheel):
     def finalize_options(self):
       _bdist_wheel.finalize_options(self)
-      self.root_is_pure = False
+      self.root_is_pure = ("--universal" in sys.argv)
 except ImportError:
   bdist_wheel = None
-  
-#Find sys/machine file
-def buildfilepath():
+
+class BaseInstallCommand(object):
+  #Install arguments (not supported by wheels) for local installation
+  #Used by --install-option
+  #  --install-option="--python-home=my_python_home"
+  user_options = [
+    ('python-home=', None, 'The Python home path'),
+    ('python-bin=', None, 'Python program name directory'),
+    ('python-lib=', None, 'Python shared library directory'),
+  ]
+
+  def initialize_options(self):
+    super().initialize_options()
+    self.python_home = ''
+    self.python_bin = ''
+    self.python_lib = ''
+    self.python_ver = f"{sys.version_info.major}.{sys.version_info.minor}"
+
+  def finalize_options(self):
+    super().finalize_options()
+
+  def run(self):
+    moduledefs = {
+      "python_home": self.python_home,
+      "python_bin": self.python_bin,
+      "python_lib": self.python_lib,
+      "python_ver": self.python_ver,
+    }
+    moduledefs_path = os.path.join(os.path.join(os.curdir, pkgname), 'moduledefs.json')
+    with open(moduledefs_path, 'w+') as openfile:
+       openfile.write(json.dumps(moduledefs))
+
+    super().run()
+
+class InstallCommand(BaseInstallCommand, install):
+  user_options = getattr(install, 'user_options', []) + BaseInstallCommand.user_options
+
+class DevelopCommand(BaseInstallCommand, develop):
+  user_options = getattr(develop, 'user_options', []) + BaseInstallCommand.user_options
+
+def get_release_version():
+  lcals = locals()
+  gbals = globals()
+  with open(os.path.join(os.getcwd(), pkgname, "__version__.py"), "rt") as opf:
+      opffilecontents = opf.read()
+      retvalue = exec(opffilecontents, gbals, lcals)
+  versorigstr = lcals["__version__"]
+  return versorigstr
+
+pkg_dir = os.path.join(os.curdir, pkgname)
+if ("sdist" in sys.argv) or (("bdist_wheel" in sys.argv) and ("--universal" in sys.argv)):
+  #sdist deploys all shared libraries
+  distutils.dir_util.copy_tree("lib", pkg_dir)
+else:
+  #Deploy the current platform shared library only
   ossys = platform.system()
-  platmacshort = ""
-  sfilename = ""
-  #print("OS:", ossys, "Machine", platmac)
+  platmac = platform.machine()
+  libdir = None
   if ossys == "Windows":
-    sfilename = "DelphiVCL.pyd"
-    #On Mac OS (and perhaps other platforms), executable files may be universal files containing multiple architectures.
-    #it is more reliable to query the sys.maxsize attribute.
-    if (sys.maxsize > 2**32): 
-      #Win x64	
-      platmacshort = "Win64"
+    if (sys.maxsize > 2**32):
+      #Win x64
+      libdir = "Win64"
     else:
       #Win x86
-      platmacshort = "Win32"
-    
-  if not platmacshort:
-    raise ValueError("Undetermined platform.")
-    
-  return f"DelphiVCL_{platmacshort}{os.sep}{sfilename}"
+      libdir = "Win32"
 
-#Copy target file from lib to pkg folder
-def copylibfiletopkg(slibfile, spkgfile): 
-  spkgdirname = os.path.dirname(spkgfile)
-  if not os.path.exists(spkgdirname):
-    os.makedirs(spkgdirname)
-  shutil.copy(slibfile, spkgfile)
-
-#Validate lib paths
-def validatelibpaths(slibdir, slibfile):
-  #print(f"Check for lib dir: {slibdir}")    
-  if not os.path.exists(f"{slibdir}"):
-    raise ValueError(f"Invalid lib path: {slibdir}")
-    
-  #print(f"Check for lib path: {slibfile}")
-  if not os.path.exists(slibfile):
-    raise ValueError(f"File not found: {slibfile}")
-  
-#Validate pkg paths
-def validatepkgpaths(spkgfile):
-  #print(f"Check for pkg path: {spkgfile}")
-  if not os.path.exists(spkgfile):
-    raise ValueError(f"File not found {spkgfile}")
-    
-#Clear pkg files (trash)
-def clearpkgtrashfiles():
-  sdir = os.path.join(os.curdir, pkgname)
-  files = os.listdir(sdir)
-  filtered_files = [file for file in files if file.endswith(".so") or file.endswith(".pyd")]
-  for file in filtered_files:
-    fpath = os.path.join(sdir, file)
-    #print("Removing trash file:", fpath)
-    os.remove(fpath)
-    
-def finddistfile():
-  sdir = os.path.join(os.curdir, pkgname)  
-  for fname in os.listdir(sdir):
-    if 'DelphiVCL' in fname:
-      return os.path.basename(fname)
-  return None  
-    
-def copylibfile():
-  spath = buildfilepath()
-  sfilename = os.path.basename(spath)
-
-  slibdir = os.path.join(os.curdir, "lib")
-  slibfile = os.path.join(slibdir, spath)
-  
-  spkgdir = os.path.join(os.curdir, pkgname)
-  spkgfile = os.path.join(spkgdir, sfilename)
- 
-  clearpkgtrashfiles()	  
-  validatelibpaths(slibdir, slibfile)
-  copylibfiletopkg(slibfile, spkgfile)
-  validatepkgpaths(spkgfile)     
-  
-  return sfilename   
-  
-def get_release_version():
-    """Creates a new version incrementing by 1 the number of build specified in the
-    DelphiVCL-0-01/__version__.py file."""
-    lcals = locals()
-    gbals = globals()
-    with open(os.path.join(os.getcwd(), pkgname, "__version__.py"), "rt") as opf:
-        opffilecontents = opf.read()
-        retvalue = exec(opffilecontents, gbals, lcals)
-    versorigstr = lcals["__version__"]
-    return versorigstr
-
-extra_args = {}
-#We don't want to share the compiled files via sdist (we don't have them)
-if not ("sdist" in sys.argv):  
-  slibdir = os.path.join(os.curdir, "lib")
-  #Binary distribution
-  if ("bdist_wheel" in sys.argv) and os.path.exists(slibdir):
-    bdata = copylibfile()
-    extra_args = {'package_data': {pkgname: [bdata]}}
+  if libdir:
+    distutils.dir_util.copy_tree(os.path.join("lib", libdir), os.path.join(pkg_dir, libdir))
   else:
-    #Final user installation
-    bdata = finddistfile()
-    if bdata:
-      extra_args = {'package_data': {pkgname: [bdata]}}      
-    
-versnewstr = get_release_version()   
+    raise ValueError("Unsupported platform.")
+
+#Create the package data.
+pkgdata = []
+for dir_, _, files in os.walk(pkg_dir):
+  for file_name in files:
+    rel_dir = os.path.relpath(dir_, pkg_dir)
+    rel_file = os.path.join(rel_dir, file_name)
+    if ''.join(Path(rel_file).suffixes) in ['.pyd']:
+      pkgdata.append(rel_file)
+print(pkgdata)
+#Read the current version from __version.py__
+versnewstr = get_release_version()
 
 with open("README.md", "r") as fh:
-  long_description = fh.read()    
+  long_description = fh.read()
 
 setuptools.setup(
   name=pkgname,
@@ -148,6 +127,9 @@ setuptools.setup(
             'Programming Language :: Python :: 3 :: Only',
             'Operating System :: Microsoft :: Windows',                        
         ],		
-  cmdclass={'bdist_wheel': bdist_wheel},
-  **extra_args
+  cmdclass={
+    'bdist_wheel': bdist_wheel,
+    'install': InstallCommand,
+    'develop': DevelopCommand,
+  }
 )
