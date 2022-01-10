c73609b36203ea9a4c88b049c2f1df6081fd8213
lmbelo <lucas.belo@live.com>
2022-01-09 19:19:12 -0800

    Extending sample to fully support VCL styles

--- samples/ActivityIndicator/activityindicator.py
+++ samples/ActivityIndicator/activityindicator.py
@@ -9,6 +9,7 @@
 #              All rights reserved
 #-------------------------------------------------------------------------------
 
+import os
 from delphivcl import *
 
 class ActivityIndicatorForm(Form):
@@ -17,7 +18,12 @@ class ActivityIndicatorForm(Form):
         self.__create_comps()
         self.__config_comps()
 
-    def __create_comps(self):
+    def __create_comps(self):   
+        self.od_styles = FileOpenDialog(self)          
+        self.mm_menu = MainMenu(self)
+        self.mi_file = self.mm_menu.CreateMenuItem() 
+        self.mi_load_style = self.mm_menu.CreateMenuItem()
+        self.mi_exit = self.mm_menu.CreateMenuItem()
         self.chk_animate = CheckBox(self)
         self.trk_frame_delay = TrackBar(self)
         self.lbl_frame_delay = Label(self)
@@ -32,6 +38,18 @@ class ActivityIndicatorForm(Form):
 
     def __config_comps(self):
         self.SetProps(ClientHeight = 328, ClientWidth = 452, Position = "poScreenCenter", OnClose = self.__on_form_close)
+        self.SetProps(Menu = self.mm_menu)  
+        #Configure the menu items   
+        self.mm_menu.Items.Insert(0, self.mi_file)
+        self.mi_file.Insert(0, self.mi_load_style)
+        self.mi_file.Insert(1, self.mi_exit)
+        #Configure the menu items
+        self.mi_file.SetProps(Caption = "File")
+        self.mi_load_style.SetProps(Caption = "Load Styles...", OnClick = self.__on_load_style_click)
+        self.mi_exit.SetProps(Caption = "Exit", OnClick = (lambda sender: Application.Terminate()))
+        #Configure the FileOpenDialog    
+        self.od_styles.SetProps(Title = "Select the Vcl Styles directory", options = ["fdoPickFolders", "fdoPathMustExist", "fdoForceFileSystem"], OkButtonLabel = "Select")        
+        #Configure further components
         self.lbl_frame_delay.SetProps(Parent = self, Left = 245, Top = 55, Width = 88, Height = 15, Caption = 'Fame Delay (50)')
         self.lbl_vcl_style.SetProps(Parent = self, Left = 20, Top = 124, Width = 49, Height = 15, Alignment = "taRightJustify", Caption = 'VCL Style')
         self.lbl_form_color.SetProps(Parent = self, Left = 245, Top = 126, Width = 176, Height = 15, Caption = 'Form Color (Windows Style Only)')
@@ -67,6 +85,22 @@ class ActivityIndicatorForm(Form):
     def __on_form_close(self, sender, action):
         action.Value = caFree
 
+    def __on_load_style_click(self, sender):
+        if self.od_styles.Execute():
+            for file in os.listdir(self.od_styles.FileName):
+                if file.endswith(".vsf"):
+                    style_file_path = os.path.join(self.od_styles.FileName, file) 
+                    valid_style, style_info = self.__sm.IsValidStyle(style_file_path)                   
+                    if valid_style and not (style_info.Name in self.cbx_vcl_style.Items):
+                        self.__sm.LoadFromFile(style_file_path) 
+
+            self.cbx_vcl_style.Items.Clear()
+            for style_name in self.__sm.StyleNames:
+                self.cbx_vcl_style.Items.Add(style_name)
+            
+        self.cbx_vcl_style.Sorted = True
+        self.cbx_vcl_style.ItemIndex = self.cbx_vcl_style.Items.IndexOf(self.__sm.ActiveStyle.Name) 
+
     def __chk_animate_click(self, sender):
         self.ai.Animate = self.chk_animate.Checked
 
@@ -84,7 +118,9 @@ class ActivityIndicatorForm(Form):
         self.ai.IndicatorColor = self.grp_indicator_color.Items[self.grp_indicator_color.ItemIndex]
 
     def __cbx_vcl_style_change(self, sender):
-        self.__sm.SetStyle(self.cbx_vcl_style.Text)
+        self.__sm.SetStyle(self.cbx_vcl_style.Text)        
+        self.lbl_form_color.Enabled = StyleServices().IsSystemStyle
+        self.cbx_form_color.Enabled = StyleServices().IsSystemStyle
 
     def __cbx_form_color_change(self, sender):
         self.Color = self.cbx_form_color.Selected
