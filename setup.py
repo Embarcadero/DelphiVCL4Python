a3f6472368d43441df057bc39160302b3d85241f
lmbelo <lucas.belo@live.com>
2023-03-07 11:27:45 -0300

    Fixing the CI to support Python 3.11

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setuptools.setup(
   license="Other/Proprietary License",
   license_files=["LICENSE.md"],
   url="https://github.com/Embarcadero/DelphiVCL4Python",
-  python_requires=">=3.7",
+  python_requires=">=3.6",
   packages=[pkgname],
   package_data={pkgname: pkgdata},
   classifiers=[            
